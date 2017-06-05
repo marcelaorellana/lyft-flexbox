a888f4240eba00167e7693aaaf4c340ff4f7490a
marcelaorellana <marcelapaz.1985@gmail.com>
2017-06-04 21:11:32 -0400

    en el formulario de agregan los otros inputs

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,29 @@ window.addEventListener("scroll", function(){
     	navi.classList.remove('cambiaNav');
     }
 });
+
+console.log(Array.from(document.getElementsByClassName("nuevoInput")).length)
+document.getElementById("numberId").addEventListener("focus", function(){
+	//si solo hay un input (phone) entonces que cree los demas inputs
+	if(Array.from(document.getElementsByClassName("nuevoInput")).length == 1){
+		var telefono = document.getElementById("numberId");
+		var nombre = document.createElement("input");
+		var email = document.createElement("input");
+		var city = document.createElement("input");
+
+		nombre.setAttribute("id", "nameId");
+		nombre.setAttribute("placeholder", "Name");
+		nombre.setAttribute("class", "nuevoInput hideInput");
+		email.setAttribute("id", "emailId");
+		email.setAttribute("placeholder", "Email");
+		email.setAttribute("class", "nuevoInput hideInput");
+		city.setAttribute("id", "cityId");
+		city.setAttribute("placeholder", "City");
+		city.setAttribute("class", "nuevoInput hideInput");
+
+		document.getElementById("inputsId").appendChild(nombre);
+		document.getElementById("inputsId").appendChild(email);
+		document.getElementById("inputsId").appendChild(city);
+
+	}
+});
